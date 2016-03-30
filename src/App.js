dde02270d5def6285f9055e2ccc579a4aa15314e
Jason Axelson <jason@hobnob.io>
2016-03-29 20:00:38 -1000

    add hobnob logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Fox from 'url!./fox.jpg'
 import column from './Trajans-Column-lower-animated.svg'
 import boxes from './boxes.svg'
+import hobnob from './hobnob_smile.svg'
 import styles from './styles.css'
 
 export class App extends Component {
@@ -11,6 +12,9 @@ export class App extends Component {
       <div style={{textAlign: 'center'}}>
         <h1>What does the fox say?</h1>
         Hmmm
+        <svg height='25' width='25'>
+          <use xlinkHref={hobnob} />
+        </svg>
         <svg>
           <use xlinkHref={column} />
         </svg>
@@ -19,6 +23,9 @@ export class App extends Component {
         </svg>
         <svg>
           <use xlinkHref={column} />
+        </svg>
+        <svg height='200' width='200'>
+          <use xlinkHref={hobnob} />
         </svg>
       </div>
     );
